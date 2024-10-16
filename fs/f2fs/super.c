686917997f3d2f01e70f061a1b660dd09329d7e4
koko <ko282ko82@gmail.com>
2024-10-16 20:08:53 +0530

    Update super.c

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -369,44 +369,6 @@ out_unlock:
 	return;
 }
 
-void f2fs_get_fsck_stat(struct f2fs_sb_info *sbi)
-{
-	struct f2fs_super_block *fsb = sbi->raw_super;
-	unsigned long long extra_flag_blk_no = le32_to_cpu(fsb->cp_blkaddr) - 1;
-
-	struct buffer_head *bh;
-	struct f2fs_sb_extra_flag_blk *extra_blk;
-
-	if (extra_flag_blk_no < 2) {
-		f2fs_warn(sbi,
-				"extra_flag: No free blks for extra flags");
-		return;
-	}
-
-	bh = sb_bread(sbi->sb, (sector_t)extra_flag_blk_no);
-	if (!bh) {
-		f2fs_warn(sbi,
-				"extra_flag: Fail to allocate buffer_head");
-		return;
-	}
-
-	extra_blk = (struct f2fs_sb_extra_flag_blk*)bh->b_data;
-	sbi->sec_fsck_stat.fsck_elapsed_time =
-			le64_to_cpu(extra_blk->fsck_elapsed_time);
-	sbi->sec_fsck_stat.fsck_read_bytes =
-			le64_to_cpu(extra_blk->fsck_read_bytes);
-	sbi->sec_fsck_stat.fsck_written_bytes =
-			le64_to_cpu(extra_blk->fsck_written_bytes);
-	sbi->sec_fsck_stat.fsck_exit_code =
-			le32_to_cpu(extra_blk->fsck_exit_code);
-	sbi->sec_fsck_stat.valid_node_count =
-			le32_to_cpu(extra_blk->valid_node_count);
-	sbi->sec_fsck_stat.valid_inode_count =
-			le32_to_cpu(extra_blk->valid_inode_count);
-
-	brelse(bh);
-}
-
 static inline void limit_reserve_root(struct f2fs_sb_info *sbi)
 {
 	block_t limit = min(sbi->user_block_count / 100,
